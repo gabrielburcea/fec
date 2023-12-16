cc825faf10b56de1ff97f22b31c361fc56617f5c
Gabriel Burcea <gb.burcea@gmail.com>
2023-12-16 15:51:23 +0800

    i have renamed mod first page to 1 because I havent got my app with the content

--- R/app_server.R
+++ R/app_server.R
@@ -8,5 +8,5 @@ app_server <- function(input, output, session) {
 
   # Load the modules
   # callModule(mod_first_page_server_1, "id_fec")
-  mod_first_page_server("mod_first_page")
+  mod_first_page_server("mod_first_page_1")
 }
